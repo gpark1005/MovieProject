73a20c51fa30beca916efd060515387833cc242e
Gerald Parker <gpark1005@gmail.com>
2021-10-14 18:50:38 -0500

    Random lines

--- handlers/router.go
+++ handlers/router.go
@@ -8,8 +8,11 @@ type Server struct {
 }
 
 func ConfigureRouter(handler MovieHandler) *mux.Router {
+	//kjhkjhkj
+	//kjhkjhkj
+	//kjhkjhkj
+	//kjhkjhkj
 	r := mux.NewRouter()
-
 	r.HandleFunc("/movie", handler.PostMovieHandler).Methods("POST")
 	r.HandleFunc("/movie/{id}", handler.GetMovieHandler).Methods("GET")
 
